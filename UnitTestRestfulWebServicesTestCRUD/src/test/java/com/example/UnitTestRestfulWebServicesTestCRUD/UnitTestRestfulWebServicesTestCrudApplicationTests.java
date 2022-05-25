b4d31f983666bb09078c56016db4ba7a156561e6
Jacek Sysiak <j4sysiak@gmail.com>
2022-05-25 07:06:45 +0200

    25-05-2022 06:10

---
+++ UnitTestRestfulWebServicesTestCRUD/src/test/java/com/example/UnitTestRestfulWebServicesTestCRUD/UnitTestRestfulWebServicesTestCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.UnitTestRestfulWebServicesTestCRUD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnitTestRestfulWebServicesTestCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

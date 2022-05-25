b39d192371285be09ec89d41c969e1e95a81a667
Jacek Sysiak <j4sysiak@gmail.com>
2022-05-25 06:15:35 +0200

    25-05-2022 06:16

--- test-controller-example/src/test/java/com/techprimers/test/testcontrollerexample/HelloResourceTest.java
+++ test-controller-example/src/test/java/com/techprimers/test/testcontrollerexample/HelloResourceTest.java
@@ -21,7 +21,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RunWith(SpringJUnit4ClassRunner.class)
 public class HelloResourceTest {
 
-
     private MockMvc mockMvc;
 
     @Mock

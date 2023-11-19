34298d3dd342d0148c3690231d93885fef2e3aec
Jacek Sysiak <j4sysiak@gmail.com>
2023-11-19 11:46:17 +0100

    20-11-2023 1146

---
+++ SimpleUnitTest/src/main/java/de/jaceksysiak/SimpleCalculator.java
@@ -0,0 +1,21 @@
+package de.jaceksysiak;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Builder
+@Getter
+@Setter
+public class SimpleCalculator {
+
+    public int add (int numbA, int numbB) {
+        return numbA + numbB;
+    }
+
+    @Override
+    public String toString() {
+        return "SimpleCalculator{}";
+    }
+
+}
---
+++ SimpleUnitTest/src/test/java/de/jaceksysiak/SimpleCalculatorTest.java
@@ -0,0 +1,10 @@
+package de.jaceksysiak;
+
+class SimpleCalculatorTest {
+
+    @org.junit.jupiter.api.Test
+    void add() {
+
+        System.out.println("ttttttttttttttttttttt");
+    }
+}

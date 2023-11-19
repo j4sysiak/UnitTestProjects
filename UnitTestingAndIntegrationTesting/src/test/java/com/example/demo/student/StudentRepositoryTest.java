86f77b515b02276e7d8ecb5f6f22465a54479b9f
Jacek Sysiak <j4sysiak@gmail.com>
2023-11-19 17:00:42 +0100

    20-11-2023 1623

--- UnitTestingAndIntegrationTesting/src/test/java/com/example/demo/student/StudentRepositoryTest.java
+++ UnitTestingAndIntegrationTesting/src/test/java/com/example/demo/student/StudentRepositoryTest.java
@@ -2,24 +2,40 @@ package com.example.demo.student;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
+@DataJpaTest
 class StudentRepositoryTest {
 
     @Autowired
     private StudentRepository underTest;
 
     @Test
-    void itShouldCheckIfStudentExistsEmail() {
+    void itShouldCheckIfStudentEmailExists() {
 
         //given
+        String mail = "jacek@wp.pl";
         Student student = new Student(
                 "Jacek",
-                "jacek@wp.pl",
+                mail,
                 Gender.FEMALE
         );
         underTest.save(student);
+
+        //when
+        boolean expected = underTest.selectExistsEmail(mail);
+
+        //then
+        assertThat(expected).isTrue();
+    }
+
+    @Test
+    void itShouldCheckIfStudentDoesNotEmailExists() {
+
+        //given
+        String mail = "jacek@wp.pl";
 
         //when
         boolean expected = underTest.selectExistsEmail("jacek@wp.pl");

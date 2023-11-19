651867ea9a2558f9a70ea55aee65588a2ebf25a5
Jacek Sysiak <j4sysiak@gmail.com>
2023-11-19 18:00:06 +0100

    20-11-2023 1623

---
+++ UnitTestingAndIntegrationTesting/src/test/java/com/example/demo/student/StudentServiceTest.java
@@ -0,0 +1,144 @@
+package com.example.demo.student;
+
+import com.example.demo.student.exception.BadRequestException;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+
+class StudentServiceTest {
+
+    @Mock
+    private StudentRepository studentRepository;
+
+    private AutoCloseable autoCloseable;
+
+    private StudentService underTest;
+
+
+    @BeforeEach
+    void setUp() {
+        autoCloseable = MockitoAnnotations.openMocks(this);
+        underTest = new StudentService(studentRepository);
+    }
+
+    @AfterEach
+    void tearDown() throws Exception {
+        autoCloseable.close();
+    }
+
+    @Test
+    void canGetAllStudents() {
+        //when
+        underTest.getAllStudents();
+
+        //then
+        verify(studentRepository).findAll();
+
+
+    }
+
+    @Test
+    void canAddStudent() {
+
+        //https://www.youtube.com/watch?v=Geq60OVyBPg   54' 23''
+
+        //given
+        String mail = "jacek@wp.pl";
+        Student student = new Student(
+                "Jacek",
+                mail,
+                Gender.FEMALE
+        );
+
+        //when
+        underTest.addStudent(student);
+
+        //then
+        ArgumentCaptor<Student> studentArgumentCaptor = ArgumentCaptor.forClass(Student.class);
+
+        verify(studentRepository).save(studentArgumentCaptor.capture());
+
+        Student capturedStudent = studentArgumentCaptor.getValue();
+
+        assertThat(capturedStudent).isEqualTo(student);
+    }
+
+    @Test
+    void willThrowExceptionWhenGivenEmailWasAllreadyTaken() {
+
+        //https://www.youtube.com/watch?v=Geq60OVyBPg   1h 03' 00''
+
+        //given
+        String mail = "jacek@wp.pl";
+        Student student = new Student(
+                "Jacek",
+                mail,
+                Gender.FEMALE
+        );
+
+        given(studentRepository.selectExistsEmail(student.getEmail()))
+                .willReturn(true);
+
+        //when & then
+        assertThatThrownBy( () -> underTest.addStudent(student) )
+                .isInstanceOf(BadRequestException.class)
+                .hasMessageContaining("Email " + student.getEmail() + " taken");
+    }
+
+    @Test
+    @Disabled
+    void deleteStudent() {
+
+        //https://www.youtube.com/watch?v=Geq60OVyBPg   1h 01' 01''
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ UnitTestingAndIntegrationTesting/src/test/java/com/example/demo/student/StudentServiceTest2.java
@@ -0,0 +1,84 @@
+package com.example.demo.student;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class StudentServiceTest2 {
+
+    @Mock
+    private StudentRepository studentRepository;
+    //private AutoCloseable autoCloseable;
+    private StudentService underTest;
+
+
+    @BeforeEach
+    void setUp() {
+        //autoCloseable = MockitoAnnotations.openMocks(this);
+        underTest = new StudentService(studentRepository);
+    }
+
+    /*
+        @AfterEach
+    void tearDown() throws Exception {
+        autoCloseable.close();
+    }
+    */
+
+
+    @Test
+    void canGetAllStudents() {
+        //when
+        underTest.getAllStudents();
+
+        //then
+        verify(studentRepository).findAll();
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

a2cb1cd064bbc2df45accba39b6e7116035d736a
Jacek Sysiak <j4sysiak@gmail.com>
2023-12-02 16:56:29 +0100

    02-12-2023 1656

---
+++ MockitoTutorialForBeginners/src/test/java/com/in28minutes/business/ListTest.java
@@ -0,0 +1,50 @@
+package com.in28minutes.business;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.mockito.Mockito.mock;
+
+public class ListTest {
+
+    @Test
+    public void letsMockListSize() {
+        List list = mock(List.class);
+        Mockito.when(list.size()).thenReturn(10);
+        assertEquals(10, list.size());
+    }
+
+//    private void assertEquals(int i, int size) {
+//    }
+
+    @Test
+    public void letsMockListSizeWithMultipleReturnValues() {
+        List list = mock(List.class);
+        Mockito.when(list.size()).thenReturn(10).thenReturn(20);
+        assertEquals(10, list.size()); // First Call
+        assertEquals(20, list.size()); // Second Call
+    }
+
+    @Test
+    public void letsMockListGet() {
+        List<String> list = mock(List.class);
+        Mockito.when(list.get(0)).thenReturn("in28Minutes");
+        assertEquals("in28Minutes", list.get(0));
+        assertNull(list.get(1));
+    }
+
+    @Test
+    public void letsMockListGetWithAny() {
+        List<String> list = mock(List.class);
+        Mockito.when(list.get(Mockito.anyInt())).thenReturn("in28Minutes");
+        // If you are using argument matchers, all arguments
+        // have to be provided by matchers.
+        assertEquals("in28Minutes", list.get(0));
+        assertEquals("in28Minutes", list.get(1));
+    }
+
+}
